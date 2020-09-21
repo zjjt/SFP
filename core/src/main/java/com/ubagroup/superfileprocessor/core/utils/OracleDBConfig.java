2f68cf4c7902c0ea2ce88c68a5ccc3542db21cd1
Jean-Jacques Zehi <jean-jacques.zehi@ubagroup.com>
2020-09-21 17:41:47 +0000

    ok sur l'autre poste de uba

--- core/src/main/java/com/ubagroup/superfileprocessor/core/utils/OracleDBConfig.java
+++ core/src/main/java/com/ubagroup/superfileprocessor/core/utils/OracleDBConfig.java
@@ -8,7 +8,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class OracleDBConfig {
-    public static String ORACLE_DRIVER="oracle.jdbc.driver.OracleDriver";
+    public static String ORACLE_DRIVER="oracle.driver.OracleDriver";
     public static String URL="jdbc:oracle:thin:@10.100.20.50:1521/UEMOAUAT";
     public static String USER="DBREAD";
     public static String PASSWORD="dbread";
